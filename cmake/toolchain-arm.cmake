8565e1e70d7265eb76d27a29ad577607978c0db7
Velho <jewel960@gmail.com>
2022-06-12 09:22:55 +0300

    Initial commit.

---
+++ cmake/toolchain-arm.cmake
@@ -0,0 +1,91 @@
+set (CMAKE_SYSTEM_NAME Generic)
+set (CMAKE_SYSTEM_PROCESSOR ARM)
+
+set (TOOLCHAIN arm-none-eabi)
+
+# Windows supported only so TOOLCHAIN_PREFIX must be passed!
+set (TOOLCHAIN_PREFIX "G:/arm/arm-toolchain/112")
+
+if (NOT DEFINED TOOLCHAIN_PREFIX)
+    if (CMAKE_HOST_SYSTEM_NAME STREQUAL Window)
+        message ("You fucked up. Please specify, TOOLCHAIN_PREFIX when running cmake. e.g. -DTOOLCHAIN_PREFIX=G:/arm/GNU Arm Embedded Toolchain/")
+        return ()
+    endif ()
+endif ()
+
+
+# Set the Directories.
+set (TOOLCHAIN_BIN_DIR ${TOOLCHAIN_PREFIX}/bin)
+set (TOOLCHAIN_INC_DIR ${TOOLCHAIN_PREFIX}/${TOOLCHAIN}/include)
+set (TOOLCHAIN_LIB_DIR ${TOOLCHAIN_PREFIX}/${TOOLCHAIN}/lib)
+
+# Set system depended extensions
+if(WIN32)
+    set(TOOLCHAIN_EXT ".exe" )
+else()
+    set(TOOLCHAIN_EXT "" )
+endif()
+
+# TODO : OBJECT_GEN_FLAGS
+# Used for setting thumbs etc.
+
+
+
+# Perform compiler test with static library
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+#---------------------------------------------------------------------------------------
+# Set compiler/linker flags
+#---------------------------------------------------------------------------------------
+
+# Object build options
+# -O0                   No optimizations, reduce compilation time and make debugging produce the expected results.
+# -mthumb               Generat thumb instructions.
+# -fno-builtin          Do not use built-in functions provided by GCC.
+# -Wall                 Print only standard warnings, for all use Wextra
+# -ffunction-sections   Place each function item into its own section in the output file.
+# -fdata-sections       Place each data item into its own section in the output file.
+# -fomit-frame-pointer  Omit the frame pointer in functions that don’t need one.
+# -mabi=aapcs           Defines enums to be a variable sized type.
+set(OBJECT_GEN_FLAGS "-O0 -mthumb -fno-builtin -Wall -ffunction-sections -fdata-sections -fomit-frame-pointer -mabi=aapcs")
+
+set(CMAKE_C_FLAGS   "${OBJECT_GEN_FLAGS} -std=gnu99 " CACHE INTERNAL "C Compiler options")
+set(CMAKE_CXX_FLAGS "${OBJECT_GEN_FLAGS} -std=c++11 " CACHE INTERNAL "C++ Compiler options")
+set(CMAKE_ASM_FLAGS "${OBJECT_GEN_FLAGS} -x assembler-with-cpp " CACHE INTERNAL "ASM Compiler options")
+
+
+# -Wl,--gc-sections     Perform the dead code elimination.
+# --specs=nano.specs    Link with newlib-nano.
+# --specs=nosys.specs   No syscalls, provide empty implementations for the POSIX system calls. -Wl,-Map=${CMAKE_PROJECT_NAME}.map
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections -mthumb -mcpu=cortex-m3 --specs=nano.specs --specs=nosys.specs -mthumb -mabi=aapcs -Wl,-Map=${CMAKE_PROJECT_NAME}.map -T ${CMAKE_SOURCE_DIR}/ldscripts/linker.ld" CACHE INTERNAL "Linker options")
+
+#---------------------------------------------------------------------------------------
+# Set debug/release build configuration Options
+#---------------------------------------------------------------------------------------
+
+# Options for DEBUG build
+# -Og   Enables optimizations that do not interfere with debugging.
+# -g    Produce debugging information in the operating system’s native format.
+set(CMAKE_C_FLAGS_DEBUG "-Og -g" CACHE INTERNAL "C Compiler options for debug build type")
+set(CMAKE_CXX_FLAGS_DEBUG "-Og -g" CACHE INTERNAL "C++ Compiler options for debug build type")
+set(CMAKE_ASM_FLAGS_DEBUG "-g" CACHE INTERNAL "ASM Compiler options for debug build type")
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG "" CACHE INTERNAL "Linker options for debug build type")
+
+# Options for RELEASE build
+# -Os   Optimize for size. -Os enables all -O2 optimizations.
+# -flto Runs the standard link-time optimizer.
+set(CMAKE_C_FLAGS_RELEASE "-Os -flto" CACHE INTERNAL "C Compiler options for release build type")
+set(CMAKE_CXX_FLAGS_RELEASE "-Os -flto" CACHE INTERNAL "C++ Compiler options for release build type")
+set(CMAKE_ASM_FLAGS_RELEASE "" CACHE INTERNAL "ASM Compiler options for release build type")
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE "-flto" CACHE INTERNAL "Linker options for release build type")
+
+# Compilers
+set(CMAKE_C_COMPILER ${TOOLCHAIN_BIN_DIR}/${TOOLCHAIN}-gcc${TOOLCHAIN_EXT} CACHE INTERNAL "C Compiler")
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_BIN_DIR}/${TOOLCHAIN}-g++${TOOLCHAIN_EXT} CACHE INTERNAL "C++ Compiler")
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN_BIN_DIR}/${TOOLCHAIN}-gcc${TOOLCHAIN_EXT} CACHE INTERNAL "ASM Compiler")
+
+set(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_PREFIX}/${${TOOLCHAIN}} ${CMAKE_PREFIX_PATH})
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
